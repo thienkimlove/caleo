6b78c04657246b4866bcb28e547b17af537c720b
Đỗ Mạnh Quân <manhquan.do@ved.com.vn>
2016-07-25 10:50:35 +0700

    update

--- resources/views/frontend/right_index.blade.php
+++ resources/views/frontend/right_index.blade.php
@@ -16,13 +16,7 @@
             @endif
         </div>
     @endif
-    @foreach ($rightBanners as $banner)
-        <div class="box-adv">
-            <a href="{{$banner->url}}">
-                <img src="{{url('files/'.$banner->image)}}" alt="Tue linh">
-            </a>
-        </div>
-    @endforeach
+
 
     <div class="boxHot cf">
         <h3 class="global-title"><a href="{{url('tin-tuc')}}">Tin nổi bật</a></h3>
